6cb0856ec5cb5d03977b3c2f159724b5a2e345f4
Alicia Key <alicia.key@galvanize.com>
2017-11-06 13:41:03 -0700

    Humps, and first route

--- routes/hoard.js
+++ routes/hoard.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const humps = require('humps')
+const knex = require('../knex')
 
 const fake = [
   {
@@ -11,8 +13,41 @@ const fake = [
 ]
 
 router.get('/', (req, res, next) => {
-  res.setHeader('Content-Type', 'application/json')
-  res.send(JSON.stringify(fake))
+  knex('hoard')
+    .select('id', 'barcode', 'name', 'description', 'img_url')
+    .orderBy('id')
+    .then((rows) => {
+      const camelized = humps.camelizeKeys(rows)
+      res.setHeader('Content-Type', 'application/json')
+      res.send(JSON.stringify(camelized))
+    })
 })
+
+/***
+router.get('/', function (req, res, next) {
+  knex('hoarding')
+    .select('id', 'barcode', 'name', 'description', 'img_url')
+    .orderBy('id')
+    .then((rows) => {
+      const camelized = humps.camelizeKeys(rows)
+      res.setHeader('Content-Type', 'application/json')
+      res.send(JSON.stringify(camelized))
+    })
+})
+
+// curl -H "Content-Type: application/json" -X POST -d '{"name": "kdjkfdjs", "description": "ksdjklsdjf", "imgUrl": "kjsdfksjdf", "barcode": "klsdjflkdsjf"}' http://localhost:8181/hoarding
+router.post('/', function (req, res, next) {
+  const { name, description, barcode, imgUrl } = req.body
+  knex('hoarding')
+    .insert({
+      name: name,
+      description: description,
+      barcode: barcode,
+      img_url: imgUrl
+    })
+    .then(() => res.sendStatus(200))
+    .catch((err) => next(err))
+})
+***/
 
 module.exports = router

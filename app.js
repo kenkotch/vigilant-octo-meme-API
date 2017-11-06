8f610d74c710d588ae0b49df2b54b75d4ec9b5c1
Alicia Key <alicia.key@galvanize.com>
2017-11-06 11:46:52 -0700

    Now ready for launch

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// CORS: Wide open
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
+
 app.use('/', index);
 app.use('/users', users);
 
